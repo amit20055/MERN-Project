5df298a48db7381fd767aef37d16f4427ac35542
Amit Prajapati <amitprajapati@gmail.com>
2026-05-03 14:10:10 +0530

    Fix MongoDB connection caching to prevent 500 errors in production

--- Backend/dbconfig.js
+++ Backend/dbconfig.js
@@ -3,13 +3,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const url = process.env.MONGO_URI;
-const dbname="My-Project2";
-export const collectionName="Todo-list";
-const client = new MongoClient(url);
+const dbname = "My-Project2";
+export const collectionName = "Todo-list";
 
- export const connection=async()=>{
-    const connect=await client.connect();
+let client = null;
+let dbInstance = null;
+
+export const connection = async () => {
+    if (dbInstance) {
+        return dbInstance;
+    }
+    client = new MongoClient(url);
+    await client.connect();
     console.log("MongoDB Connected");
-    return await connect.db(dbname)
-
+    dbInstance = client.db(dbname);
+    return dbInstance;
 }
